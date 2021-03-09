49ff97e475d94634f3cf88e574dc0550d1049e12
Purevbaatar Otgonbaatar <purveeuka@gmail.com>
2021-03-09 12:15:41 +0100

    relpaced 'dishwasher' with' galaxy'

---
+++ week-02/day-2/src/SimpleReplace.java
@@ -0,0 +1,10 @@
+
+public class SimpleReplace {
+    public static void main(String... args){
+        String original = "In a dishwasher far far away";
+        System.out.println(original.replace("dishwasher", "galaxy"));
+    }
+}
+// I would like to replace "dishwasher" with "galaxy" in this example variable, but it has a problem
+// Please fix it for me!
+// Expected output: In a galaxy far far away
