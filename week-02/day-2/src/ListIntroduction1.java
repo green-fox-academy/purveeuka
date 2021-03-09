548f34687c0cb7fbeee5ded2222e9f126d570b21
Purevbaatar Otgonbaatar <purveeuka@gmail.com>
2021-03-09 15:31:13 +0100

    Added missing steps

--- week-02/day-2/src/ListIntroduction1.java
+++ week-02/day-2/src/ListIntroduction1.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
-
+import java.util.Collections;
 
 public class ListIntroduction1 {
     public static void main(String[] args) {
@@ -20,13 +20,29 @@ public class ListIntroduction1 {
         System.out.println(names.get(2));
         System.out.println("------------");
 
-        for (String i: names) {
+        for (String i : names) {
             System.out.println(i);
         }
+        int j = 1;
+        for (String i : names) {
+            System.out.println(j + ". " + i);
+            j++;
+        }
 
+        names.remove(1);
+        System.out.println(names);
+        System.out.println("------------");
+
+        Collections.reverse(names);
+        for (String i : names) {
+            System.out.println(i);
+        }
+        System.out.println("------------");
+        names.removeAll(names);
+        System.out.println(names);
     }
-
 }
+
 /*
     Create an empty list which will contain names (strings)
     Print out the number of elements in the list
@@ -37,4 +53,12 @@ public class ListIntroduction1 {
         Print out the number of elements in the list
         Print out the 3rd element
         Iterate through the list and print out each name
- */
+
+        Iterate through the list and print
+           1. William
+           2. John
+           3. Amanda
+        Remove the 2nd element
+        Iterate through the list in a reversed order and print out each name
+        Remove all elements
+*/
