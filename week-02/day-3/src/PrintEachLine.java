cf14377eec26f66203de0e6fbca51a97c7452842
Purevbaatar Otgonbaatar <purveeuka@gmail.com>
2021-03-10 12:20:53 +0100

    created funtion that reads .txt and throws error message if it the file doesn't exist

---
+++ week-02/day-3/src/PrintEachLine.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class PrintEachLine {
+    public static void main(String[] args) {
+
+        try {
+            Path myFilePath = Paths.get("src/my-file.txt");
+            List<String> lines = Files.readAllLines(myFilePath);
+            for (String string:lines) {
+                System.out.println(string);
+            }
+        } catch (IOException e) {
+            System.out.println("Unable to read file: my-file.txt");
+        }
+    }
+}
+// Write a program that opens a file called "my-file.txt" and prints each
+// line from the file.
+// If the program is unable to read the file (for example the file does not exist),
+// it should print the following error message: "Unable to read file: my-file.txt".
