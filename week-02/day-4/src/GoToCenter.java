bc617ed5e76df6b8ed76689354d04a36c08a5876
Purevbaatar Otgonbaatar <purveeuka@gmail.com>
2021-03-11 14:16:28 +0100

    drew 3 lines points to the centre

---
+++ week-02/day-4/src/GoToCenter.java
@@ -0,0 +1,42 @@
+import javax.swing.*;
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+public class GoToCenter {
+    public static void drawImage(Graphics graphics) {
+        // Create a function that takes 3 parameters and draws a single line
+        // Parameters: the x and y coordinates of the line's starting point and the graphics
+        // The function shall draw a line from that point to the center of the canvas
+        // Draw at least 3 lines with that function using a loop
+        drawToCenter(800, 600, graphics);
+    }
+    public static void drawToCenter(int x, int y, Graphics graphics) {
+        for (int lines = 0; lines < 3; lines++) {
+            graphics.drawLine(lines * 10 + x, lines * 20 + y, WIDTH / 2, HEIGHT / 2);
+        }
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            drawImage(graphics);
+        }
+    }
+
+}
