8d5e6faf118035e02b015ba285aa99158efe1d51
Purevbaatar Otgonbaatar <purveeuka@gmail.com>
2021-03-15 15:22:43 +0100

    created a recursion that counts even/odd numbers of ears that bunnies have

---
+++ week-03/day-1/src/BunniesAgain.java
@@ -0,0 +1,23 @@
+public class BunniesAgain {
+    public static void main(String[] args) {
+        System.out.println(numOfEars(10));
+    }
+    public static int numOfEars( int bunniesInLine) {
+        if (bunniesInLine == 0) {
+            return 0;
+        } else {
+            if ( bunniesInLine  % 2 == 0){
+                return 2 + numOfEars(bunniesInLine -1);
+        } else {
+                return 3 + numOfEars(bunniesInLine -1);
+            }
+        }
+    }
+}
+// We have bunnies standing in a line,
+// numbered 1, 2, 3, 4, ...
+// The odd bunnies (1, 3, ...) have the normal 2 ears.
+// The even bunnies (2, 4, ..) we'll say have 3 ears,
+// because they each have a raised foot.
+// Recursively return the number of "ears" in
+// the bunny line 1, 2, ... n (without loops or multiplication).
