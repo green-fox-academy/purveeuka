a15cd7a06a8d47cb489d2b5262e84647d70f969e
Purevbaatar Otgonbaatar <purveeuka@gmail.com>
2021-03-15 15:26:58 +0100

    created a recursion that find largest element of an array

---
+++ week-03/day-1/src/MaximumFinder.java
@@ -0,0 +1,14 @@
+public class MaximumFinder {
+    public static void main(String[] args) {
+        int[] array = {10, 20, 30, 60, 50};
+        int numbers = array.length;
+        System.out.println(findLargest(array, numbers));
+    }
+    public static int findLargest(int[] arr, int number){
+        if (number == 1) {
+            return arr[0];
+        }
+        return Math.max(arr[number - 1], findLargest(arr, number - 1));
+    }
+}
+// Write a function that finds the largest element of an array using recursion.
