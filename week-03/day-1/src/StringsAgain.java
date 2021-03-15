ce4f5c1c82b92e4af18f290c4b707f5e14ac6f72
Purevbaatar Otgonbaatar <purveeuka@gmail.com>
2021-03-15 15:32:27 +0100

    created recursion that removes x chars

---
+++ week-03/day-1/src/StringsAgain.java
@@ -0,0 +1,14 @@
+public class StringsAgain {
+    public static void main(String[] args) {
+        System.out.println(removeX("Hxappy birthxday tox youx!"));
+
+    }
+    private static String removeX (String string) {
+        if (string.contains("x")) {
+            return removeX(string.replaceFirst("x",""));
+        } return string;
+    }
+}
+// Given a string,
+// compute recursively a new string where
+// all the 'x' chars have been removed.
