50a8d91a0b1517c47b983ec1a2dfbd05b30bf360
Purevbaatar Otgonbaatar <purveeuka@gmail.com>
2021-03-15 16:16:15 +0100

    created recursaion that adds number from 1 to given number

---
+++ week-03/day-1/src/recursion/NumberAdder.java
@@ -0,0 +1,17 @@
+package recursion;
+
+public class NumberAdder {
+    public static void main(String[] args) {
+        System.out.println(numAdder(6));
+    }
+
+    public static int numAdder(int n) {
+        if( n == 1) {
+            return n;
+        }
+        return numAdder(n-1) + n;
+    }
+}
+
+// Write a recursive function that takes one parameter: n and
+// adds numbers from 1 to n.
