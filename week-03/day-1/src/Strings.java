95effc6387f36a49dd82c26e716c8510301a6666
Purevbaatar Otgonbaatar <purveeuka@gmail.com>
2021-03-15 15:31:26 +0100

    created a recursion that replaces x chars with y

---
+++ week-03/day-1/src/Strings.java
@@ -0,0 +1,19 @@
+public class Strings {
+    public static void main(String[] args) {
+        String changedChars = "xXxxX";
+        System.out.println(changedChars(changedChars));
+    }
+
+    public static String  changedChars(String string) {
+        if (string.length() == 0) {
+            return string;
+        } else if (string.charAt(0) == 'x'){
+            return 'y' + changedChars(string.substring(1));
+        }
+        else {
+            return string.charAt(0) + changedChars(string.substring(1));
+        }
+    }
+}
+// Given a string, compute recursively (no loops) a new string where
+// all the lowercase 'x' chars have been changed to 'y' chars.
