4c5fc167d9993abf68d13ede12bf2e97db005235
Purevbaatar Otgonbaatar <purveeuka@gmail.com>
2021-03-15 15:33:47 +0100

    created a recursion that saparates adjacent chars with *

---
+++ week-03/day-1/src/StringsAgainAndAgain.java
@@ -0,0 +1,16 @@
+public class StringsAgainAndAgain {
+    public static void main(String[] args) {
+        System.out.println(adjacent("Hello there!", "Hello there!".length()));
+    }
+
+    public static String adjacent( String string, int length){
+        if (length > 1) {
+            string = string.substring(0,length - 1) + "*" + string.substring(length - 1);
+            return adjacent(string, length - 1);
+        }
+        return string;
+    }
+}
+// Given a string,
+// compute recursively a new string where
+// all the adjacent chars are now separated by a `*`
