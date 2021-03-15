ffc278d42e7b11b324149c145d48a09e73ba2336
Purevbaatar Otgonbaatar <purveeuka@gmail.com>
2021-03-15 15:36:34 +0100

    created a recursion that finds the greatest common divisor of two given numbers

---
+++ week-03/day-1/src/GreatestCommonDivisor.java
@@ -0,0 +1,20 @@
+public class GreatestCommonDivisor {
+    public static void main(String[] args) {
+        int aNum = 6;
+        int bNum = 4;
+        System.out.println("The greatest common divisor of " + aNum + " and " + bNum + " is "+ gcd(aNum, bNum) + ".");
+    }
+    public static int gcd ( int aNum, int bNum) {
+        if (aNum == 0) {
+            return bNum;
+        }
+        if (bNum == 0) {
+            return aNum;
+        }
+        if (aNum > bNum) {
+            return gcd(aNum - bNum, bNum);
+        }
+        return gcd(aNum, bNum - aNum);
+    }
+}
+// Find the greatest common divisor of two numbers using recursion.
