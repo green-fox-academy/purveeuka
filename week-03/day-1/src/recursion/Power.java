527cd27a336147415572afae8625c8680f82bc76
Purevbaatar Otgonbaatar <purveeuka@gmail.com>
2021-03-15 16:17:09 +0100

    created a recursion that find power two given parameters

---
+++ week-03/day-1/src/recursion/Power.java
@@ -0,0 +1,16 @@
+package recursion;
+
+public class Power {
+    public static void main(String[] args) {
+        System.out.println(powerN(3, 2));
+    }
+    public static int powerN(int base, int number) {
+        if (number == 0) {
+            return 1;
+        } else {
+            return base * powerN(base, number -1);
+        }
+    }
+    }
+// Given base and n that are both 1 or more, compute recursively (no loops) the
+// value of base to the n power, so powerN(3, 2) is 9 (3 squared).
