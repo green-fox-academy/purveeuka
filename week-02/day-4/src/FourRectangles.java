cd4de2a90adc64d7cdefe525ee8cb3da3e08a5ba
Purevbaatar Otgonbaatar <purveeuka@gmail.com>
2021-03-16 16:32:59 +0100

    improved some coding

--- week-02/day-4/src/FourRectangles.java
+++ week-02/day-4/src/FourRectangles.java
@@ -7,18 +7,20 @@ import static javax.swing.JFrame.EXIT_ON_CLOSE;
 public class FourRectangles {
 
     public static void drawImage(Graphics graphics){
-     int x = 20;
-     int y = 10;
-        for (int shape = 0; shape < 4; shape++) {
-            Random random = new Random();
-            graphics.setColor(new Color(random.nextInt(0xFFFFFF)));
-            graphics.fillRect(x, y, x, y);
-            x = random.nextInt(50);
-            y = random.nextInt(100);
-        }
+         fourRectangle(graphics,12, 20);
         // Draw four different size and color rectangles
         // Avoid code duplication!
     }
+    public  static void fourRectangle(Graphics graphics, int x, int y) {
+        for (int shapes = 0; shapes < 4; shapes++) {
+            Random random = new Random();
+            graphics.setColor(new Color(random.nextInt(0xFFFFFF)));
+            graphics.fillRect( x, y, x, y);
+            x = random.nextInt(100);
+            y = random.nextInt(100);
+        }
+    }
+
 
     // Don't touch the code below
     static int WIDTH = 320;
