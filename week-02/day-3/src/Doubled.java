ba7507fcf798a75a5361a621c0a285e4e83d738a
Purevbaatar Otgonbaatar <purveeuka@gmail.com>
2021-03-16 16:20:09 +0100

    created a method that decrypts the text

---
+++ week-02/day-3/src/Doubled.java
@@ -0,0 +1,38 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+public class Doubled {
+    public static void main(String[] args) {
+        decryptText("src/textFiles/duplicated-chars.txt");
+    }
+    // Create a method that decrypts the duplicated-chars.txt.
+    // Decryption is the process reversing an encryption, i.e. the process
+    // which converts encrypted data into its original form.
+
+    public static void decryptText (String fileName) {
+        ArrayList<String> decryptedLines = new ArrayList<>();
+        try {
+            Path filepath = Paths.get(fileName);
+            List<String> lines = Files.readAllLines(filepath);
+            for (int chars = 0; chars < lines.size(); chars++) {
+                String temporary = lines.get(chars);
+                String temporary2 = "";
+                for (int j = 0; j < temporary.length() ; j++) {
+                    if (j % 2 == 0)
+                        temporary2 = temporary2 + String.valueOf(temporary.charAt(j));
+                } decryptedLines.add(chars, temporary2);
+            }
+            Files.write(filepath, decryptedLines);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
