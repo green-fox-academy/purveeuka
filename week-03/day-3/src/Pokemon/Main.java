66360ee01a1a571e5e43b4f95a9b6a605ecd7db5
Purevbaatar Otgonbaatar <purveeuka@gmail.com>
2021-03-18 08:57:06 +0100

    added a for each loop to choose suitable pokemon

---
+++ week-03/day-3/src/Pokemon/Main.java
@@ -0,0 +1,39 @@
+package Pokemon;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Pokemon> pokemonListOfAsh = initializePokemons();
+
+        // Every pokemon has a name and a type.
+        // Certain types are effective against others, e.g. water is effective against fire.
+
+        // Ash has a few pokemon.
+        // A wild pokemon appeared!
+
+        Pokemon wildPokemon = new Pokemon("Oddish", "leaf", "water");
+
+        // Which pokemon should Ash use?
+        // Write the necessary code to get the name of the right pokemon and print it:
+        for (Pokemon chosen1:pokemonListOfAsh) {
+            if (chosen1.isEffectiveAgainst(wildPokemon)) {
+                System.out.print("I choose you, " + chosen1.name);
+            }
+        }
+    }
+
+    private static List<Pokemon> initializePokemons() {
+        List<Pokemon> pokemon = new ArrayList<>();
+
+        pokemon.add(new Pokemon("Balbasaur", "leaf", "water"));
+        pokemon.add(new Pokemon("Pikatchu", "electric", "water"));
+        pokemon.add(new Pokemon("Charizard", "fire", "leaf"));
+        pokemon.add(new Pokemon("Balbasaur", "water", "fire"));
+        pokemon.add(new Pokemon("Kingler", "water", "fire"));
+
+        return pokemon;
+    }
+}
+
