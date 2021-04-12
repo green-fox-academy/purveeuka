6e6c9b6722cf6f39975484db9445019a52650d38
Purevbaatar Otgonbaatar <purveeuka@gmail.com>
2021-04-12 17:19:01 +0200

    adding exercise 12

---
+++ week-06/day-1/src/Exercise12/StarWars.java
@@ -0,0 +1,79 @@
+package Exercise12;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class StarWars {
+    public static void main(String[] args) {
+     List<String> lines = readFile("src/Text-Files/ swcharacters.csv");
+     List<SwCharacter> swCharacters = lines.stream().map(SwCharacter::new).collect(Collectors.toList());
+
+     //Print the name of the heaviest character (if the mass is unknown, ignore that character)
+     Optional<SwCharacter> heaviestCharacter = swCharacters.stream()
+             .max(Comparator.comparingInt(SwCharacter::getMass));
+     heaviestCharacter.ifPresent(System.out ::println);
+     System.out.println("-------------------------------------------------");
+
+     /*
+     Print the average height of the male characters
+     Print the average height of the female characters
+     */
+     System.out.println(getAvgHeightOf(swCharacters, "male"));
+     System.out.println("-------------------------------------------------");
+     System.out.println(getAvgHeightOf(swCharacters, "female"));
+     System.out.println("-------------------------------------------------");
+
+     /*
+     Get the age distribution of the characters by gender (where the gender can be "male", "female" and "other")
+     The age groups are: "below 21", "between 21 and 40", "above 40" and "unknown"
+     The result should be a Map<String, Map<String, Integer>>
+     */
+     Map<String, Map<String, Integer>> ageAndGender = swCharacters
+             .stream()
+             .collect(
+                     Collectors.groupingBy((SwCharacter ch) -> ch.getGender(),
+                             Collectors.groupingBy(
+                                     (SwCharacter ch) -> ageGroup(ch.getAge()),
+                                     Collectors.summingInt(ch -> 1)
+                             )
+                     )
+             );
+        System.out.println(ageAndGender);
+    }
+
+    public static double getAvgHeightOf(List<SwCharacter> swCharacters, String gender) {
+        return swCharacters
+                .stream()
+                .filter(swCharacter -> swCharacter.getGender().equals(gender))
+                .mapToDouble(SwCharacter::getHeight)
+                .summaryStatistics()
+                .getAverage();
+    }
+
+    public static String ageGroup(int age) {
+        if (age < 21) {
+            return "below 21";
+        } else if (age <= 40) {
+            return "between 21 and 40";
+        } else if (age > 40) {
+            return "above 40";
+        } else {
+            return "unknown";
+        }
+    }
+
+    private static List<String> readFile(String fileName){
+        List<String> fileContent = new ArrayList<>();
+        Path path = Path.of(fileName);
+        try {
+            fileContent = Files.readAllLines(path);
+            fileContent.remove(0);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return fileContent;
+    }
+}
---
+++ week-06/day-1/src/Exercise12/SwCharacter.java
@@ -0,0 +1,62 @@
+package Exercise12;
+
+import java.util.regex.Pattern;
+
+public class SwCharacter {
+    private static final Pattern numPattern = Pattern.compile("^[0-9]+$");
+    private static final Pattern datePattern = Pattern.compile("^[0-9]+BBY$");
+    private final String name;
+    private final int height;
+    private final int mass;
+    private final String gender;
+    private final int age;
+
+    public SwCharacter(String line) {
+        String[] data = line.split(";");
+        this.name = data[0];
+        this.height = numValidator(data[1]);
+        this.mass = numValidator(data[2]);
+        this.gender = genderValidator(data[7]);
+        this.age = dateValidator(data[6]);
+
+    }
+
+    private int dateValidator(String data) {
+        return (datePattern.matcher(data).find()) ? (Integer.parseInt(data.replace("BBY", ""))) : (1);
+    }
+
+    private String genderValidator(String data) {
+        return (data.equals("male") || data.equals("female") ? data : "other");
+    }
+
+    private int numValidator(String data) {
+        return numPattern.matcher(data).find() ? Integer.parseInt(data) : 1;
+
+    }
+
+    @Override
+    public String toString() {
+        return this.name + ", " + this.age + " years old " + this.gender + ", Height: " + this.height + "cm Mass: " + this.mass + "kg";
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getMass() {
+        return mass;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
