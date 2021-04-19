13f4c5f3f86e63685130b1ef0ca95ab87712412e
Purevbaatar Otgonbaatar <purveeuka@gmail.com>
2021-04-19 17:37:28 +0200

    adding basic web shop

---
+++ week-06/week-05/BasicWebshop/src/test/java/com/greenfox/basicwebshop/BasicWebshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenfox.basicwebshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicWebshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
